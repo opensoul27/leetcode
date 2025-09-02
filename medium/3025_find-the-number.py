90ea7b6e423bb7a906ed18979b19690ef16668b5
opensoul27 <miroslavkhusnutdinov@gmail.com>
2025-09-02 16:53:53 +0300

    Add: 3025. Find the Number of Ways to Place People I (Medium)

---
+++ medium/3025_find-the-number.py
@@ -0,0 +1,36 @@
+
+"""
+LeetCode: 3025. Find the Number of Ways to Place People I
+URL: https://leetcode.com/problems/3025_find-the-number/
+Difficulty: Medium
+"""
+
+class Solution:
+    def numberOfPairs(self, points: List[List[int]]) -> int:
+        res = 0
+        n = len(points)
+
+        for i in range(n):
+            x1, y1 = points[i]
+            for j in range(n):
+                if i == j:
+                    continue
+                x2, y2 = points[j]
+                        
+                if x1 > x2 or y1 < y2:
+                    continue
+
+                flag = True
+                for k in range(n):
+                    if k == i or k == j:
+                        continue
+                    x, y = points[k]
+                    if x >= x1 and x <= x2 and y <= y1 and y >= y2:
+                        flag = False
+                        break
+
+                if flag:
+                    res += 1
+        return res
+
+
