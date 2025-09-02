7b38d45b20d24115f0338f898a85a58ebf2bfc78
opensoul27 <miroslavkhusnutdinov@gmail.com>
2025-09-02 16:08:22 +0300

    Add: Palindrome Number (Easy)

---
+++ easy/palindrome-number.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+"""
+LeetCode: Palindrome Number
+URL: https://leetcode.com/problems/palindrome-number/
+Difficulty: Easy
+"""
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        return str(x)[::-1] == str(x)
+
+
